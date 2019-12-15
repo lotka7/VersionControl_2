66fe0ec56f03239c03701ef007d0b3469a8275f2
Knon Máté <knon.mate7@gmail.com>
2019-12-15 11:34:32 +0100

    plus

--- UserMaintance/UserMaintance/Form1.cs
+++ UserMaintance/UserMaintance/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintance.Entities;
 
 namespace UserMaintance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +22,20 @@ namespace UserMaintance
             label1.Text = Resource1.lastName;
             label2.Text = Resource1.firstName;
             button1.Text = Resource1.add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "fullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            User u = new User()
+            {
+                lastName = label1.Text,
+                firstName = label2.Text
+            };
+            users.Add(u);
         }
     }
 }
